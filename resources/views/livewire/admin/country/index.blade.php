2083f4e0f4126b35805dffed0e6dc99a58510fea
Mehdi Afrasiabi <mr.abbann1976@gmail.com>
2025-02-16 11:34:54 +0330

    Implement admin template assets 2

---
+++ resources/views/livewire/admin/country/index.blade.php
@@ -0,0 +1,76 @@
+<div class="widget widget-chart-two">
+    <div class="widget-heading">
+        <h5 class="">فروش بر اساس دسته بندی</h5>
+    </div>
+    <div class="widget-content" style="position: relative;">
+        <div id="chart-2" class="" style="min-height: 380px;"><div id="apexcharts418xk8nf" class="apexcharts-canvas apexcharts418xk8nf light" style="width: 380px; height: 380px;"><svg id="SvgjsSvg1817" width="380" height="380" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" class="apexcharts-svg" xmlns:data="ApexChartsNS" transform="translate(0, 0)" style="background: transparent;"><foreignObject x="0" y="0" width="380" height="380"><div class="apexcharts-legend center position-bottom" xmlns="http://www.w3.org/1999/xhtml" style="inset: auto 0px 10px; position: absolute;"><div class="apexcharts-legend-series" rel="1" data:collapsed="false" style="margin: 0px 8px;"><span class="apexcharts-legend-marker" rel="1" data:collapsed="false" style="background: rgb(92, 26, 195); color: rgb(92, 26, 195); height: 10px; width: 10px; left: 0px; top: 0px; border-width: 0px; border-color: rgb(255, 255, 255); border-radius: 12px;"></span><span class="apexcharts-legend-text" rel="1" data:collapsed="false" style="color: rgb(55, 61, 63); font-size: 14px; font-family: Helvetica, Arial, sans-serif;">پوشاک</span></div><div class="apexcharts-legend-series" rel="2" data:collapsed="false" style="margin: 0px 8px;"><span class="apexcharts-legend-marker" rel="2" data:collapsed="false" style="background: rgb(226, 160, 63); color: rgb(226, 160, 63); height: 10px; width: 10px; left: 0px; top: 0px; border-width: 0px; border-color: rgb(255, 255, 255); border-radius: 12px;"></span><span class="apexcharts-legend-text" rel="2" data:collapsed="false" style="color: rgb(55, 61, 63); font-size: 14px; font-family: Helvetica, Arial, sans-serif;">الکترونیک</span></div><div class="apexcharts-legend-series" rel="3" data:collapsed="false" style="margin: 0px 8px;"><span class="apexcharts-legend-marker" rel="3" data:collapsed="false" style="background: rgb(231, 81, 90); color: rgb(231, 81, 90); height: 10px; width: 10px; left: 0px; top: 0px; border-width: 0px; border-color: rgb(255, 255, 255); border-radius: 12px;"></span><span class="apexcharts-legend-text" rel="3" data:collapsed="false" style="color: rgb(55, 61, 63); font-size: 14px; font-family: Helvetica, Arial, sans-serif;">دیگر</span></div></div><style type="text/css">
+
+                            .apexcharts-legend {
+                                display: flex;
+                                overflow: auto;
+                                padding: 0 10px;
+                            }
+
+                            .apexcharts-legend.position-bottom, .apexcharts-legend.position-top {
+                                flex-wrap: wrap
+                            }
+                            .apexcharts-legend.position-right, .apexcharts-legend.position-left {
+                                flex-direction: column;
+                                bottom: 0;
+                            }
+
+                            .apexcharts-legend.position-bottom.left, .apexcharts-legend.position-top.left, .apexcharts-legend.position-right, .apexcharts-legend.position-left {
+                                justify-content: flex-start;
+                            }
+
+                            .apexcharts-legend.position-bottom.center, .apexcharts-legend.position-top.center {
+                                justify-content: center;
+                            }
+
+                            .apexcharts-legend.position-bottom.right, .apexcharts-legend.position-top.right {
+                                justify-content: flex-end;
+                            }
+
+                            .apexcharts-legend-series {
+                                cursor: pointer;
+                                line-height: normal;
+                            }
+
+                            .apexcharts-legend.position-bottom .apexcharts-legend-series, .apexcharts-legend.position-top .apexcharts-legend-series{
+                                display: flex;
+                                align-items: center;
+                            }
+
+                            .apexcharts-legend-text {
+                                position: relative;
+                                font-size: 14px;
+                            }
+
+                            .apexcharts-legend-text *, .apexcharts-legend-marker * {
+                                pointer-events: none;
+                            }
+
+                            .apexcharts-legend-marker {
+                                position: relative;
+                                display: inline-block;
+                                cursor: pointer;
+                                margin-right: 3px;
+                            }
+
+                            .apexcharts-legend.right .apexcharts-legend-series, .apexcharts-legend.left .apexcharts-legend-series{
+                                display: inline-block;
+                            }
+
+                            .apexcharts-legend-series.no-click {
+                                cursor: auto;
+                            }
+
+                            .apexcharts-legend .apexcharts-hidden-zero-series, .apexcharts-legend .apexcharts-hidden-null-series {
+                                display: none !important;
+                            }
+
+                            .inactive-legend {
+                                opacity: 0.45;
+                            }</style></foreignObject><g id="SvgjsG1819" class="apexcharts-inner apexcharts-graphical" transform="translate(41.383803367614746, 0)"><defs id="SvgjsDefs1818"><clipPath id="gridRectMask418xk8nf"><rect id="SvgjsRect1820" width="324.2323932647705" height="336.2323932647705" x="-12.5" y="-12.5" rx="0" ry="0" fill="#ffffff" opacity="1" stroke-width="0" stroke="none" stroke-dasharray="0"></rect></clipPath><clipPath id="gridRectMarkerMask418xk8nf"><rect id="SvgjsRect1821" width="301.2323932647705" height="313.2323932647705" x="-1" y="-1" rx="0" ry="0" fill="#ffffff" opacity="1" stroke-width="0" stroke="none" stroke-dasharray="0"></rect></clipPath></defs><g id="SvgjsG1823" class="apexcharts-pie" data:innerTranslateX="0" data:innerTranslateY="-25"><g id="SvgjsG1825" class="apexcharts-datalabels-group" transform="translate(0, 0)" style="opacity: 1;"><text id="SvgjsText1826" font-family="Nunito, sans-serif" x="149.61619663238525" y="139.61619663238525" text-anchor="middle" dominant-baseline="auto" font-size="29px" fill="#888ea8" class="apexcharts-datalabel-label" style="font-family: Nunito, sans-serif; fill: rgb(136, 142, 168);">مجموع</text><text id="SvgjsText1827" font-family="Nunito, sans-serif" x="149.61619663238525" y="181.61619663238525" text-anchor="middle" dominant-baseline="auto" font-size="26px" fill="#bfc9d4" class="apexcharts-datalabel-value" style="font-family: Nunito, sans-serif;">1992</text></g><g id="SvgjsG1824" transform="translate(0, -5) scale(1)"><circle id="SvgjsCircle1828" r="76.02856371809797" cx="149.61619663238525" cy="149.61619663238525" fill="transparent"></circle><g id="SvgjsG1829"><g id="apexcharts-series-0" class="apexcharts-series apexcharts-pie-series" seriesName="پوشاک" rel="1"><path id="apexcharts-donut-slice-0" d="M 149.61619663238525 32.64917552761915 A 116.9670211047661 116.9670211047661 0 0 1 153.67370579360878 266.5128203882893 L 152.25357758718056 225.5990020737229 A 76.02856371809797 76.02856371809797 0 0 0 149.61619663238525 73.58763291428728 L 149.61619663238525 32.64917552761915 z" fill="rgba(92,26,195,1)" fill-opacity="1" stroke="#0e1726" stroke-opacity="1" stroke-linecap="butt" stroke-width="25" stroke-dasharray="0" class="apexcharts-pie-area" index="0" j="0" data:angle="178.0120481927711" data:startAngle="0" data:strokeWidth="25" data:value="985" data:pathOrig="M 149.61619663238525 32.64917552761915 A 116.9670211047661 116.9670211047661 0 0 1 153.67370579360878 266.5128203882893 L 152.25357758718056 225.5990020737229 A 76.02856371809797 76.02856371809797 0 0 0 149.61619663238525 73.58763291428728 L 149.61619663238525 32.64917552761915 z"></path></g><g id="apexcharts-series-1" class="apexcharts-series apexcharts-pie-series" seriesName="الکترونیک" rel="2"><path id="apexcharts-donut-slice-1" d="M 153.67370579360878 266.5128203882893 A 116.9670211047661 116.9670211047661 0 0 1 61.61494683918143 72.56385039324117 L 92.41538426680276 99.53217157694161 A 76.02856371809797 76.02856371809797 0 0 0 152.25357758718056 225.5990020737229 L 153.67370579360878 266.5128203882893 z" fill="rgba(226,160,63,1)" fill-opacity="1" stroke="#0e1726" stroke-opacity="1" stroke-linecap="butt" stroke-width="25" stroke-dasharray="0" class="apexcharts-pie-area" index="0" j="1" data:angle="133.19277108433732" data:startAngle="178.0120481927711" data:strokeWidth="25" data:value="737" data:pathOrig="M 153.67370579360878 266.5128203882893 A 116.9670211047661 116.9670211047661 0 0 1 61.61494683918143 72.56385039324117 L 92.41538426680276 99.53217157694161 A 76.02856371809797 76.02856371809797 0 0 0 152.25357758718056 225.5990020737229 L 153.67370579360878 266.5128203882893 z"></path></g><g id="apexcharts-series-2" class="apexcharts-series apexcharts-pie-series" seriesName="دیگر" rel="3"><path id="apexcharts-donut-slice-2" d="M 61.61494683918143 72.56385039324117 A 116.9670211047661 116.9670211047661 0 0 1 149.5957820361436 32.649177309128746 L 149.6029271448282 73.58763407226853 A 76.02856371809797 76.02856371809797 0 0 0 92.41538426680276 99.53217157694161 L 61.61494683918143 72.56385039324117 z" fill="rgba(231,81,90,1)" fill-opacity="1" stroke="#0e1726" stroke-opacity="1" stroke-linecap="butt" stroke-width="25" stroke-dasharray="0" class="apexcharts-pie-area" index="0" j="2" data:angle="48.79518072289159" data:startAngle="311.2048192771084" data:strokeWidth="25" data:value="270" data:pathOrig="M 61.61494683918143 72.56385039324117 A 116.9670211047661 116.9670211047661 0 0 1 149.5957820361436 32.649177309128746 L 149.6029271448282 73.58763407226853 A 76.02856371809797 76.02856371809797 0 0 0 92.41538426680276 99.53217157694161 L 61.61494683918143 72.56385039324117 z"></path></g></g></g></g><line id="SvgjsLine1836" x1="0" y1="0" x2="299.2323932647705" y2="0" stroke="#b6b6b6" stroke-dasharray="0" stroke-width="1" class="apexcharts-ycrosshairs"></line><line id="SvgjsLine1837" x1="0" y1="0" x2="299.2323932647705" y2="0" stroke-dasharray="0" stroke-width="0" class="apexcharts-ycrosshairs-hidden"></line></g></svg><div class="apexcharts-tooltip dark" style="left: 268.145px; top: 118.258px;"><div class="apexcharts-tooltip-series-group active" style="display: flex; background-color: rgb(92, 26, 195);"><span class="apexcharts-tooltip-marker" style="background-color: rgb(92, 26, 195); display: none;"></span><div class="apexcharts-tooltip-text" style="font-family: Helvetica, Arial, sans-serif; font-size: 12px;"><div class="apexcharts-tooltip-y-group"><span class="apexcharts-tooltip-text-label">پوشاک: </span><span class="apexcharts-tooltip-text-value">985.0</span></div><div class="apexcharts-tooltip-z-group"><span class="apexcharts-tooltip-text-z-label"></span><span class="apexcharts-tooltip-text-z-value"></span></div></div></div><div class="apexcharts-tooltip-series-group" style="display: none; background-color: rgb(92, 26, 195);"><span class="apexcharts-tooltip-marker" style="background-color: rgb(92, 26, 195); display: none;"></span><div class="apexcharts-tooltip-text" style="font-family: Helvetica, Arial, sans-serif; font-size: 12px;"><div class="apexcharts-tooltip-y-group"><span class="apexcharts-tooltip-text-label">پوشاک: </span><span class="apexcharts-tooltip-text-value">985.0</span></div><div class="apexcharts-tooltip-z-group"><span class="apexcharts-tooltip-text-z-label"></span><span class="apexcharts-tooltip-text-z-value"></span></div></div></div><div class="apexcharts-tooltip-series-group" style="display: none; background-color: rgb(92, 26, 195);"><span class="apexcharts-tooltip-marker" style="background-color: rgb(92, 26, 195); display: none;"></span><div class="apexcharts-tooltip-text" style="font-family: Helvetica, Arial, sans-serif; font-size: 12px;"><div class="apexcharts-tooltip-y-group"><span class="apexcharts-tooltip-text-label">پوشاک: </span><span class="apexcharts-tooltip-text-value">985.0</span></div><div class="apexcharts-tooltip-z-group"><span class="apexcharts-tooltip-text-z-label"></span><span class="apexcharts-tooltip-text-z-value"></span></div></div></div></div></div></div>
+        <div class="resize-triggers"><div class="expand-trigger"><div style="width: 449px; height: 401px;"></div></div><div class="contract-trigger"></div></div></div>
+</div>
---
+++ resources/views/livewire/admin/layout/sidebar.blade.php
@@ -0,0 +1,140 @@
+<div class="sidebar-wrapper sidebar-theme">
+
+    <nav id="sidebar">
+        <div class="shadow-bottom"></div>
+        <ul class="list-unstyled menu-categories ps ps--active-y" id="accordionExample">
+            <li class="menu">
+                <a href="#dashboard" data-active="true" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle collapsed">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                        <span>داشبورد</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
+                </a>
+                <ul class="submenu list-unstyled collapse" id="dashboard" data-parent="#accordionExample" style="">
+                    <li class="active">
+                        <a href="index.html"> فروش ها </a>
+                    </li>
+                    <li>
+                        <a href="index2.html"> تجزیه و تحلیل </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="menu">
+                <a href="#app" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-cpu"><rect x="4" y="4" width="16" height="16" rx="2" ry="2"></rect><rect x="9" y="9" width="6" height="6"></rect><line x1="9" y1="1" x2="9" y2="4"></line><line x1="15" y1="1" x2="15" y2="4"></line><line x1="9" y1="20" x2="9" y2="23"></line><line x1="15" y1="20" x2="15" y2="23"></line><line x1="20" y1="9" x2="23" y2="9"></line><line x1="20" y1="14" x2="23" y2="14"></line><line x1="1" y1="9" x2="4" y2="9"></line><line x1="1" y1="14" x2="4" y2="14"></line></svg>
+                        <span>دسته بندی</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
+                </a>
+            </li>
+
+            <li class="menu">
+                <a href="#components" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
+                        <span>محصولات</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
+                </a>
+                <ul class="collapse submenu list-unstyled" id="components" data-parent="#accordionExample">
+
+                    <li>
+                        <a href="component_lightbox.html">افزودن</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="menu">
+                <a href="#elements" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-zap"><polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon></svg>
+                        <span>سفارشات</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
+                </a>
+                <ul class="collapse submenu list-unstyled" id="elements" data-parent="#accordionExample">
+                    <li>
+                        <a href="element_typography.html"> تایپوگرافی </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="menu">
+                <a href="table_basic.html" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layout"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg>
+                        <span>تراکنش ها</span>
+                    </div>
+                </a>
+            </li>
+
+            <li class="menu">
+                <a href="#datatables" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layers"><polygon points="12 2 2 7 12 12 22 7 12 2"></polygon><polyline points="2 17 12 22 22 17"></polyline><polyline points="2 12 12 17 22 12"></polyline></svg>
+                        <span>مپ</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
+                </a>
+                <ul class="collapse submenu list-unstyled" id="datatables" data-parent="#accordionExample">
+                    <li>
+                        <a href="table_dt_basic.html">کشور ها</a>
+                    </li>
+                    <li>
+                        <a href="table_dt_basic-light.html">استان ها</a>
+                    </li>
+                    <li>
+                        <a href="table_dt_ordering_sorting.html">شهر</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu">
+                <a href="#users" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                        <span>کاربران</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
+                </a>
+                <ul class="collapse submenu list-unstyled" id="users" data-parent="#accordionExample">
+                    <li>
+                        <a href="user_profile.html"> پروفایل </a>
+                    </li>
+                    <li>
+                        <a href="user_account_setting.html"> تنظیمات اکانت </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="menu">
+                <a href="#forms" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                    <div class="">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
+                        <span>تنظیمات</span>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                    </div>
+                </a>
+            </li>
+
+            <div class="ps__rail-x" style="left: 0px; bottom: 0px;"><div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div></div><div class="ps__rail-y" style="top: 0px; height: 279px; right: 206px;"><div class="ps__thumb-y" tabindex="0" style="top: 0px; height: 88px;"></div></div></ul>
+        <!-- <div class="shadow-bottom"></div> -->
+
+    </nav>
+
+</div>
